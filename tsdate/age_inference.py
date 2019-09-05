06b2aedb2685c75cad8b28ee6ed2fa9b01c98923
Wilder Wohns <awohns@gmail.com>
2019-09-05 15:19:55 +0100

    fixed return_ts() in age_inference.py

--- tsdate/age_inference.py
+++ tsdate/age_inference.py
@@ -374,9 +374,15 @@ def return_ts(ts, vals, Ne):
     Output new inferred tree sequence with node ages assigned.
     """
     tables = ts.dump_tables()
-    tables.nodes.time = vals * 2 * Ne
+    tables.nodes.set_columns(
+        flags=tables.nodes.flags,
+        time=vals * 2 * Ne,
+        population=tables.nodes.population,
+        individual=tables.nodes.individual,
+        metadata=tables.nodes.metadata,
+        metadata_offset=tables.nodes.metadata_offset)
     tables.sort()
-    return tables.tree_sequence()
+    return(tables.tree_sequence())
 
 
 def age_inference(
