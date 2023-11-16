183363af4a7cc2bdab5710c04f79afc3fa59d30b
Nate Pope <nspope@utexas.edu>
2023-11-16 12:19:21 -0800

    Import parameter_grid

--- tsdate/__init__.py
+++ tsdate/__init__.py
@@ -23,6 +23,7 @@ from .cache import *  # NOQA: F401,F403
 from .core import date  # NOQA: F401
 from .core import get_dates  # NOQA: F401
 from .prior import build_grid as build_prior_grid  # NOQA: F401
+from .prior import parameter_grid as build_parameter_grid  # NOQA: F401
 from .provenance import __version__  # NOQA: F401
 from .util import add_sampledata_times  # NOQA: F401
 from .util import preprocess_ts  # NOQA: F401
