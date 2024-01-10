Isolate ExpectationPropagation class; add machinery for ancient samples; numerical fixes when hypergeometric argument is close to unity; add utilities to split nodes
Linting

Disable prior entirely if prior_mixture_dim=0

Minor cleanup

Fix error catching issue

tests/test_approximations.py
@@ -36,12 +36,13 @@ from tsdate import approx
 from tsdate import hypergeo
 from tsdate import prior
 
+# TODO: better test set?
+# TODO: test special case where child is fixed to age 0
 _gamma_trio_test_cases = [  # [shape1, rate1, shape2, rate2, muts, rate]
-    [10.541, 0.0005, 10.552, 0.005, 1.0, 0.0151],
-    [10.541, 0.0065, 10.552, 0.005, 1.0, 0.0101],
-    [10.541, 0.0065, 10.552, 0.022, 1.0, 0.0051],
-    [10.541, 0.0265, 10.552, 0.022, 1.0, 0.0051],
-    [4, 4, 4, 4, 4, 4],
+    [2.0, 0.0005, 2.0, 0.005, 0.0, 0.001],
+    [2.0, 0.0005, 2.0, 0.005, 1.0, 0.001],
+    [2.0, 0.0005, 2.0, 0.005, 2.0, 0.001],
+    [2.0, 0.0005, 2.0, 0.005, 3.0, 0.001],
 ]
 
 
@@ -70,110 +71,221 @@ class TestPosteriorMomentMatching:
                 * np.exp(-(t_i - t_j) * mu)
             )
 
-    def test_sufficient_statistics(self, pars):
-        logconst, t_i, ln_t_i, t_j, ln_t_j = approx.sufficient_statistics(*pars)
-        ck_normconst = scipy.integrate.dblquad(
-            lambda ti, tj: self.pdf(ti, tj, *pars),
-            0,
-            np.inf,
-            lambda tj: tj,
-            np.inf,
-            epsabs=0,
-        )[0]
-        assert np.isclose(logconst, np.log(ck_normconst), rtol=2e-3)
-        ck_t_i = scipy.integrate.dblquad(
-            lambda ti, tj: ti * self.pdf(ti, tj, *pars) / ck_normconst,
-            0,
-            np.inf,
-            lambda tj: tj,
-            np.inf,
-            epsabs=0,
-        )[0]
-        assert np.isclose(t_i, ck_t_i, rtol=2e-3)
-        ck_t_j = scipy.integrate.dblquad(
-            lambda ti, tj: tj * self.pdf(ti, tj, *pars) / ck_normconst,
-            0,
-            np.inf,
-            lambda tj: tj,
-            np.inf,
-            epsabs=0,
-        )[0]
-        assert np.isclose(t_j, ck_t_j, rtol=2e-3)
-        ck_ln_t_i = scipy.integrate.dblquad(
-            lambda ti, tj: np.log(ti) * self.pdf(ti, tj, *pars) / ck_normconst,
-            0,
-            np.inf,
-            lambda tj: tj,
-            np.inf,
-            epsabs=0,
-        )[0]
-        assert np.isclose(ln_t_i, ck_ln_t_i, rtol=2e-3)
-        ck_ln_t_j = scipy.integrate.dblquad(
-            lambda ti, tj: np.log(tj) * self.pdf(ti, tj, *pars) / ck_normconst,
-            0,
-            np.inf,
-            lambda tj: tj,
-            np.inf,
-            epsabs=0,
-        )[0]
-        assert np.isclose(ln_t_j, ck_ln_t_j, rtol=2e-3)
+    @staticmethod
+    def pdf_rootward(t_i, t_j, a_i, b_i, y, mu):
+        """
+        Target conditional distribution, proportional to the parent
+        marginals (gamma) and a Poisson mutation likelihood at a
+        fixed child age
+        """
+        if t_i < t_j:
+            return 0.0
+        else:
+            return (
+                t_i ** (a_i - 1)
+                * np.exp(-t_i * b_i)
+                * (t_i - t_j) ** y
+                * np.exp(-(t_i - t_j) * mu)
+            )
 
-    def test_taylor_approximation(self, pars):
-        logconst, t_i, _, var_t_i, t_j, _, var_t_j = approx.taylor_approximation(*pars)
+    @staticmethod
+    def pdf_leafward(t_i, t_j, a_j, b_j, y, mu):
+        """
+        Target conditional distribution, proportional to the child
+        marginals (gamma) and a Poisson mutation likelihood at a
+        fixed parent age
+        """
+        if t_i < t_j:
+            return 0.0
+        else:
+            return (
+                t_j ** (a_j - 1)
+                * np.exp(-t_j * b_j)
+                * (t_i - t_j) ** y
+                * np.exp(-(t_i - t_j) * mu)
+            )
+
+    @staticmethod
+    def pdf_truncated(t_i, low, upp, a_i, b_i):
+        """
+        Target proportional to the node marginals (gamma) and an indicator
+        function
+        """
+        if low < t_i < upp:
+            return np.exp(
+                np.log(t_i) * (a_i - 1)
+                - t_i * b_i
+                - scipy.special.gammaln(a_i)
+                + np.log(b_i) * a_i
+            )
+        else:
+            return 0.0
+
+    def test_moments(self, pars):
+        """
+        Test mean and variance when ages of both nodes are free
+        """
+        logconst, t_i, _, var_t_i, t_j, _, var_t_j = approx.moments(*pars)
         ck_normconst = scipy.integrate.dblquad(
-            lambda ti, tj: self.pdf(ti, tj, *pars),
+            lambda t_i, t_j: self.pdf(t_i, t_j, *pars),
             0,
             np.inf,
-            lambda tj: tj,
+            lambda t_j: t_j,
             np.inf,
             epsabs=0,
         )[0]
-        assert np.isclose(logconst, np.log(ck_normconst), rtol=2e-3)
+        assert np.isclose(logconst, np.log(ck_normconst), rtol=2e-2)
         ck_t_i = scipy.integrate.dblquad(
-            lambda ti, tj: ti * self.pdf(ti, tj, *pars) / ck_normconst,
+            lambda t_i, t_j: t_i * self.pdf(t_i, t_j, *pars) / ck_normconst,
             0,
             np.inf,
-            lambda tj: tj,
+            lambda t_j: t_j,
             np.inf,
             epsabs=0,
         )[0]
-        assert np.isclose(t_i, ck_t_i, rtol=2e-3)
+        assert np.isclose(t_i, ck_t_i, rtol=2e-2)
         ck_t_j = scipy.integrate.dblquad(
-            lambda ti, tj: tj * self.pdf(ti, tj, *pars) / ck_normconst,
+            lambda t_i, t_j: t_j * self.pdf(t_i, t_j, *pars) / ck_normconst,
             0,
             np.inf,
-            lambda tj: tj,
+            lambda t_j: t_j,
             np.inf,
             epsabs=0,
         )[0]
-        assert np.isclose(t_j, ck_t_j, rtol=2e-3)
+        assert np.isclose(t_j, ck_t_j, rtol=2e-2)
         ck_var_t_i = (
             scipy.integrate.dblquad(
-                lambda ti, tj: ti**2 * self.pdf(ti, tj, *pars) / ck_normconst,
+                lambda t_i, t_j: t_i**2 * self.pdf(t_i, t_j, *pars) / ck_normconst,
                 0,
                 np.inf,
-                lambda tj: tj,
+                lambda t_j: t_j,
                 np.inf,
                 epsabs=0,
             )[0]
             - ck_t_i**2
         )
-        assert np.isclose(var_t_i, ck_var_t_i, rtol=1e-2)
+        assert np.isclose(var_t_i, ck_var_t_i, rtol=2e-2)
         ck_var_t_j = (
             scipy.integrate.dblquad(
-                lambda ti, tj: tj**2 * self.pdf(ti, tj, *pars) / ck_normconst,
+                lambda t_i, t_j: t_j**2 * self.pdf(t_i, t_j, *pars) / ck_normconst,
                 0,
                 np.inf,
-                lambda tj: tj,
+                lambda t_j: t_j,
                 np.inf,
                 epsabs=0,
             )[0]
             - ck_t_j**2
         )
-        assert np.isclose(var_t_j, ck_var_t_j, rtol=1e-2)
+        assert np.isclose(var_t_j, ck_var_t_j, rtol=2e-2)
 
-    def test_approximate_gamma(self, pars):
-        _, t_i, ln_t_i, t_j, ln_t_j = approx.sufficient_statistics(*pars)
+    def test_rootward_moments(self, pars):
+        """
+        Test mean and variance of parent age when child age is fixed to a nonzero value
+        """
+        a_i, b_i, a_j, b_j, y, mu = pars
+        t_j = a_j / b_j  # point "estimate" for child
+        pars_redux = (a_i, b_i, y, mu)
+        logconst, t_i, _, var_t_i = approx.rootward_moments(t_j, *pars_redux)
+        ck_normconst = scipy.integrate.quad(
+            lambda t_i: self.pdf_rootward(t_i, t_j, *pars_redux),
+            t_j,
+            np.inf,
+            epsabs=0,
+        )[0]
+        assert np.isclose(logconst, np.log(ck_normconst), rtol=2e-2)
+        ck_t_i = scipy.integrate.quad(
+            lambda t_i: t_i * self.pdf_rootward(t_i, t_j, *pars_redux) / ck_normconst,
+            t_j,
+            np.inf,
+            epsabs=0,
+        )[0]
+        assert np.isclose(t_i, ck_t_i, rtol=2e-2)
+        ck_var_t_i = (
+            scipy.integrate.quad(
+                lambda t_i: t_i**2
+                * self.pdf_rootward(t_i, t_j, *pars_redux)
+                / ck_normconst,
+                t_j,
+                np.inf,
+                epsabs=0,
+            )[0]
+            - ck_t_i**2
+        )
+        assert np.isclose(var_t_i, ck_var_t_i, rtol=2e-2)
+
+    def test_leafward_moments(self, pars):
+        """
+        Test mean and variance of child age when parent age is fixed to a nonzero value
+        """
+        a_i, b_i, a_j, b_j, y, mu = pars
+        t_i = a_i / b_i  # point "estimate" for parent
+        pars_redux = (a_j, b_j, y, mu)
+        logconst, t_j, _, var_t_j = approx.leafward_moments(t_i, *pars_redux)
+        ck_normconst = scipy.integrate.quad(
+            lambda t_j: self.pdf_leafward(t_i, t_j, *pars_redux),
+            0,
+            t_i,
+            epsabs=0,
+        )[0]
+        assert np.isclose(logconst, np.log(ck_normconst), rtol=2e-2)
+        ck_t_j = scipy.integrate.quad(
+            lambda t_j: t_j * self.pdf_leafward(t_i, t_j, *pars_redux) / ck_normconst,
+            0,
+            t_i,
+            epsabs=0,
+        )[0]
+        assert np.isclose(t_j, ck_t_j, rtol=2e-2)
+        ck_var_t_j = (
+            scipy.integrate.quad(
+                lambda t_j: t_j**2
+                * self.pdf_leafward(t_i, t_j, *pars_redux)
+                / ck_normconst,
+                0,
+                t_i,
+                epsabs=0,
+            )[0]
+            - ck_t_j**2
+        )
+        assert np.isclose(var_t_j, ck_var_t_j, rtol=2e-2)
+
+    def test_truncated_moments(self, pars):
+        """
+        Test mean and variance of child age when parent age is fixed to a nonzero value
+        """
+        a_i, b_i, *_ = pars
+        upp = a_i / b_i * 2
+        low = a_i / b_i / 2
+        pars_redux = (low, upp, a_i, b_i)
+        logconst, t_i, _, var_t_i = approx.truncated_moments(*pars_redux)
+        ck_normconst = scipy.integrate.quad(
+            lambda t_i: self.pdf_truncated(t_i, *pars_redux),
+            low,
+            upp,
+            epsabs=0,
+        )[0]
+        assert np.isclose(logconst, np.log(ck_normconst), rtol=1e-4)
+        ck_t_i = scipy.integrate.quad(
+            lambda t_i: t_i * self.pdf_truncated(t_i, *pars_redux) / ck_normconst,
+            low,
+            upp,
+            epsabs=0,
+        )[0]
+        assert np.isclose(t_i, ck_t_i, rtol=1e-4)
+        ck_var_t_i = (
+            scipy.integrate.quad(
+                lambda t_i: t_i**2
+                * self.pdf_truncated(t_i, *pars_redux)
+                / ck_normconst,
+                low,
+                upp,
+                epsabs=0,
+            )[0]
+            - ck_t_i**2
+        )
+        assert np.isclose(var_t_i, ck_var_t_i, rtol=1e-4)
+
+    def test_approximate_gamma_kl(self, pars):
+        _, t_i, ln_t_i, _, t_j, ln_t_j, _ = approx.moments(*pars)
         alpha_i, beta_i = approx.approximate_gamma_kl(t_i, ln_t_i)
         alpha_j, beta_j = approx.approximate_gamma_kl(t_j, ln_t_j)
         ck_t_i = (alpha_i + 1) / beta_i
@@ -184,6 +296,19 @@ class TestPosteriorMomentMatching:
         assert np.isclose(ln_t_i, ck_ln_t_i)
         ck_ln_t_j = hypergeo._digamma(alpha_j + 1) - np.log(beta_j)
         assert np.isclose(ln_t_j, ck_ln_t_j)
+
+    def test_approximate_gamma_mom(self, pars):
+        _, t_i, _, va_t_i, t_j, _, va_t_j = approx.moments(*pars)
+        alpha_i, beta_i = approx.approximate_gamma_mom(t_i, va_t_i)
+        alpha_j, beta_j = approx.approximate_gamma_mom(t_j, va_t_j)
+        ck_t_i = (alpha_i + 1) / beta_i
+        assert np.isclose(t_i, ck_t_i)
+        ck_t_j = (alpha_j + 1) / beta_j
+        assert np.isclose(t_j, ck_t_j)
+        ck_va_t_i = (alpha_i + 1) / beta_i**2
+        assert np.isclose(va_t_i, ck_va_t_i)
+        ck_va_t_j = (alpha_j + 1) / beta_j**2
+        assert np.isclose(va_t_j, ck_va_t_j)
 
 
 class TestPriorMomentMatching:

tests/test_hypergeo.py
@@ -11,8 +11,7 @@
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in
-# all
-# copies or substantial portions of the Software.
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -24,8 +23,6 @@
 """
 Test cases for numba-fied hypergeometric functions
 """
-import itertools
-
 import mpmath
 import numdifftools as nd
 import numpy as np
@@ -56,13 +53,21 @@ class TestPolygamma:
         )
 
 
-@pytest.mark.parametrize("a_i", [1.0, 10.0, 100.0, 1000.0])
-@pytest.mark.parametrize("b_i", [0.001, 0.01, 0.1, 1.0])
-@pytest.mark.parametrize("a_j", [1.0, 10.0, 100.0, 1000.0])
-@pytest.mark.parametrize("b_j", [0.001, 0.01, 0.1, 1.0])
-@pytest.mark.parametrize("y", [0.0, 1.0, 10.0, 1000.0])
-@pytest.mark.parametrize("mu", [0.005, 0.05, 0.5, 5.0])
-class TestLaplaceApprox:
+@pytest.mark.parametrize(
+    "pars",
+    [
+        [969.5, 0.1062, 984.72, 0.1215, 0.0, 0.000239],
+        [897.8, 0.0954, 936.03, 0.1042, 1.0, 0.000840],
+        [37.94, 0.0087, 413.54, 0.1122, 0.0, 2.70e-05],
+        [748.1, 0.0857, 544.65, 0.0621, 0.0, 0.000511],
+        [21.18, 0.0226, 21.898, 0.0292, 0.0, 0.000618],
+        [7.931, 0.0010, 1000.0, 0.1675, 0.0, 0.000290],
+        [77.15, 0.0484, 42.007, 0.0348, 0.0, 0.000277],
+        [990.5, 0.1318, 130.68, 0.0205, 1.0, 0.000245],
+        [678.9, 0.1184, 121.71, 0.0958, 2.0, 0.000208],
+    ],
+)
+class TestLaplaceApprox2F1:
     """
     Test that Laplace approximation to 2F1 returns reasonable answers
     """
@@ -76,122 +81,132 @@ class TestLaplaceApprox:
         val = mpmath.re(mpmath.hyp2f1(A, B, C, z, maxterms=1e6))
         return val / offset
 
-    def test_2f1(self, a_i, b_i, a_j, b_j, y, mu):
-        pars = [a_i, b_i, a_j, b_j, y, mu]
+    def test_2f1(self, pars):
+        a_i, b_i, a_j, b_j, y, mu = pars
         A = a_j
         B = a_i + a_j + y
         C = a_j + y + 1
         z = (mu - b_j) / (mu + b_i)
-        f, *_ = hypergeo._hyp2f1(*pars)
-        ff = hypergeo._hyp2f1_fast(A, B, C, z)
+        f = hypergeo._hyp2f1_laplace(A, B, C, z)
         check = float(mpmath.log(self._2f1_validate(*pars)))
-        assert np.isclose(f, ff)
         assert np.isclose(f, check, rtol=2e-2)
 
-    def test_grad(self, a_i, b_i, a_j, b_j, y, mu):
-        pars = [a_i, b_i, a_j, b_j, y, mu]
-        _, *grad = hypergeo._hyp2f1(*pars)
-        da_i = nd.Derivative(
-            lambda a_i: hypergeo._hyp2f1(a_i, b_i, a_j, b_j, y, mu)[0], step=1e-3
-        )
-        db_i = nd.Derivative(
-            lambda b_i: hypergeo._hyp2f1(a_i, b_i, a_j, b_j, y, mu)[0], step=1e-5
-        )
-        da_j = nd.Derivative(
-            lambda a_j: hypergeo._hyp2f1(a_i, b_i, a_j, b_j, y, mu)[0], step=1e-3
-        )
-        db_j = nd.Derivative(
-            lambda b_j: hypergeo._hyp2f1(a_i, b_i, a_j, b_j, y, mu)[0], step=1e-5
-        )
-        check = [da_i(a_i), db_i(b_i), da_j(a_j), db_j(b_j)]
-        assert np.allclose(grad, check, rtol=1e-3)
-
-
-# ------------------------------------------------- #
-# The routines below aren't used in tsdate anymore, #
-# but may be useful in the future                   #
-# ------------------------------------------------- #
-
 
 @pytest.mark.parametrize(
     "pars",
-    list(
-        itertools.product(
-            [0.8, 20.0, 200.0],
-            [1.9, 90.3, 900.3],
-            [1.6, 30.7, 300.7],
-            [0.0, 0.1, 0.45],
-        )
-    ),
+    [
+        [202.865, 0.0153, 0.0012724, 101353547.00, 0.0, 2.404560e-05],
+        [999.998, 0.1041, 0.0012612, 14749022.000, 2.0, 7.830300e-05],
+        [157.778, 0.0182, 0.0011993, 20861958.000, 0.0, 4.128000e-07],
+        [1000.00, 0.1431, 0.0009718, 22638776.000, 0.0, 5.881110e-05],
+        [405.482, 0.1484, 0.0009711, 41053809.000, 0.0, 6.229410e-05],
+        [115.773, 0.7506, 0.0015549, 23315696857.0, 0.0, 3.601680e-05],
+        [73.4909, 1.0600, 0.0010028, 56254280956.0, 0.0, 0.0002302392],
+        [18.9981, 0.1315, 0.0015053, 23443723634.0, 1.0, 2.470348e-05],
+    ],
 )
-class TestTaylorSeries:
+class Test2F1Unity:
     """
-    Test Taylor series expansions of 2F1
+    As TestLaplaceApprox2F1 but in the argument-close-to-unity limit
     """
 
     @staticmethod
-    def _2f1_validate(a, b, c, z, offset=1.0):
-        val = mpmath.re(mpmath.hyp2f1(a, b, c, z))
+    def _2f1_validate(a_i, b_i, a_j, b_j, y, mu, offset=1.0):
+        A = a_j
+        B = a_i + a_j + y
+        C = a_j + y + 1
+        z = (mu - b_j) / (mu + b_i)
+        val = mpmath.re(mpmath.hyp2f1(A, B, C, z, maxterms=1e7))
         return val / offset
 
-    @staticmethod
-    def _2f1_grad_validate(a, b, c, z, offset=1.0):
-        p = [a, b, c, z]
-        grad = nd.Gradient(
-            lambda x: float(TestTaylorSeries._2f1_validate(*x, offset=offset)),
-            step=1e-7,
-            richardson_terms=4,
-        )
-        return grad(p)
-
     def test_2f1(self, pars):
-        f, *_ = hypergeo._hyp2f1_taylor_series(*pars)
-        check = self._2f1_validate(*pars)
-        assert np.isclose(f, float(mpmath.log(mpmath.fabs(check))))
-
-    def test_2f1_grad(self, pars):
-        _, *grad = hypergeo._hyp2f1_taylor_series(*pars)
-        offset = self._2f1_validate(*pars)
-        check = self._2f1_grad_validate(*pars, offset=offset)
-        assert np.allclose(grad, check)
+        a_i, b_i, a_j, b_j, y, mu = pars
+        A = a_j
+        B = a_i + a_j + y
+        C = a_j + y + 1
+        z = (mu - b_j) / (mu + b_i)
+        s = 0.0
+        if z < 0.0:
+            s = -B * np.log(1 - z)
+            A = C - A
+            z = z / (z - 1)
+        f = hypergeo._hyp2f1_unity(A, B, C, z) + s
+        check = float(mpmath.log(self._2f1_validate(*pars)))
+        assert np.isclose(f, check, rtol=1e-2)
 
 
 @pytest.mark.parametrize(
     "pars",
-    list(
-        itertools.product(
-            [-130.1, 20.0, 200.0],
-            [-20.2, 90.3, 900.3],
-            [-1.6, 30.7, 300.7],
-            [-3.0, -0.5, 0.0, 0.1, 0.9],
-        )
-    ),
+    [
+        [92.301156, 3.338, 0.008172, 0.0, 0.00022691],
+        [92.301156, 3.678, 0.005831, 0.0, 0.00250921],
+        [2998.6825, 3.999, 0.000750, 1.0, 0.00098396],
+        [11985.278, 5.251, 0.000193, 8.0, 1.9537e-05],
+        [11985.278, 14.46, 0.000822, 3.0, 0.00011632],
+        [12957.347, 0.999, 9.999e-5, 1.0, 0.00019150],
+    ],
 )
-class TestCheckValid2F1:
+class TestLaplaceApproxU:
     """
-    Test the check for 2F1 series convergence, that plugs derivatives into the
-    differential equation that defines 2F1
+    Test that Laplace approximation to Tricomi's confluent hypergeometric
+    function returns reasonable answers
     """
 
     @staticmethod
-    def _2f1(a, b, c, z):
-        val = mpmath.re(mpmath.hyp2f1(a, b, c, z))
-        dz = a * b / c * mpmath.re(mpmath.hyp2f1(a + 1, b + 1, c + 1, z))
-        d2z = (
-            a
-            * b
-            / c
-            * (a + 1)
-            * (b + 1)
-            / (c + 1)
-            * mpmath.re(mpmath.hyp2f1(a + 2, b + 2, c + 2, z))
-        )
-        return float(dz / val), float(d2z / val)
+    def _hyperu_validate(t_j, a_i, b_i, y, mu, offset=1.0):
+        A = y + 1
+        B = a_i + y + 1
+        z = t_j * (mu + b_i)
+        val = mpmath.re(mpmath.hyperu(A, B, z, maxterms=1e6))
+        return val / offset
 
-    def test_is_valid_2f1(self, pars):
-        dz, d2z = self._2f1(*pars)
-        assert hypergeo._is_valid_2f1(dz, d2z, *pars, 1e-10)
-        # perturb solution to differential equation
-        dz *= 1 + 1e-3
-        d2z *= 1 - 1e-3
-        assert not hypergeo._is_valid_2f1(dz, d2z, *pars, 1e-10)
+    def test_hyperu(self, pars):
+        t_j, a_i, b_i, y, mu = pars
+        A = y + 1
+        B = a_i + y + 1
+        z = t_j * (mu + b_i)
+        f, df = hypergeo._hyperu_laplace(A, B, z)
+        der = nd.Derivative(
+            lambda z: hypergeo._hyperu_laplace(A, B, z)[0], n=1, step=1e-4
+        )
+        check_f = float(mpmath.log(self._hyperu_validate(*pars)))
+        check_df = der(z)
+        assert np.isclose(f, check_f, rtol=2e-2)
+        assert np.isclose(df, check_df, rtol=2e-2)
+
+
+@pytest.mark.parametrize(
+    "pars",
+    [
+        [25128.46, 132.48, 0.006849, 0.0, 4.4562e-05],
+        [15900.89, 997.09, 0.093978, 1.0, 0.00010288],
+        [10062.80, 978.24, 0.102753, 0.0, 0.00022747],
+        [11659.45, 713.52, 0.064035, 8.0, 0.00064625],
+        [40888.01, 999.99, 0.071372, 3.0, 3.7925e-05],
+        [11659.45, 989.08, 0.096664, 2.0, 0.00010425],
+        [22333.94, 28.546, 0.001817, 0.0, 9.6225e-05],
+        [30613.25, 30.956, 0.000942, 0.0, 1.1250e-07],
+    ],
+)
+class TestLaplaceApproxM:
+    """
+    Test that Laplace approximation to Kummer's confluent hypergeometric
+    function returns reasonable answers
+    """
+
+    @staticmethod
+    def _hyp1f1_validate(t_i, a_j, b_j, y, mu, offset=1.0):
+        A = a_j
+        B = a_j + y + 1
+        z = t_i * (b_j - mu)
+        val = mpmath.re(mpmath.hyp1f1(A, B, z, maxterms=1e6))
+        return val / offset
+
+    def test_hyp1f1(self, pars):
+        t_i, a_j, b_j, y, mu = pars
+        A = a_j
+        B = a_j + y + 1
+        z = t_i * (b_j - mu)
+        f = hypergeo._hyp1f1_laplace(A, B, z)
+        check = float(mpmath.log(self._hyp1f1_validate(*pars)))
+        assert np.isclose(f, check, rtol=2e-2)

tsdate/mixture.py
@@ -25,12 +25,28 @@ Mixture of gamma distributions that may be fit via EM to distribution-valued obs
 """
 import numba
 import numpy as np
+from numba.types import Tuple as _tuple
+from numba.types import UniTuple as _unituple
 
 from . import approx
 from . import hypergeo
 
+# shorthand for numba readonly array types, [type][dimension][constness]
+# type is one of "i" (int32), "f" (float64)
+# dimension is a nonzero integer
+# constness is one of "r" (read-only) or "w" (writable)
+_f = numba.types.float64
+_i = numba.types.int32
+_b = numba.types.bool_
+_f1w = numba.types.Array(_f, 1, "C", readonly=False)
+_f1r = numba.types.Array(_f, 1, "C", readonly=True)
+_f2w = numba.types.Array(_f, 2, "C", readonly=False)
+_f2r = numba.types.Array(_f, 2, "C", readonly=True)
+_f3w = numba.types.Array(_f, 3, "C", readonly=False)
+_i1r = numba.types.Array(_i, 1, "C", readonly=True)
 
-@numba.njit("UniTuple(f8[:], 4)(f8[:], f8[:], f8[:], f8, f8)")
+
+@numba.njit(_unituple(_f1w, 4)(_f1r, _f1r, _f1r, _f, _f))
 def _conditional_posterior(prior_logweight, prior_alpha, prior_beta, alpha, beta):
     r"""
     Return expectations of node age :math:`t` from the mixture model,
@@ -82,7 +98,7 @@ def _conditional_posterior(prior_logweight, prior_alpha, prior_beta, alpha, beta
     return E, E_t, E_logt, E_tlogt
 
 
-@numba.njit("f8(f8[:], f8[:], f8[:], f8[:], f8[:])")
+@numba.njit(_f(_f1w, _f1w, _f1w, _f1r, _f1r))
 def _em_update(prior_weight, prior_alpha, prior_beta, alpha, beta):
     """
     Perform an expectation maximization step for parameters of mixture components,
@@ -138,7 +154,7 @@ def _em_update(prior_weight, prior_alpha, prior_beta, alpha, beta):
     return loglik
 
 
-@numba.njit("f8[:](f8[:], f8[:], f8[:], f8[:], f8[:])")
+@numba.njit(_f1w(_f1r, _f1r, _f1r, _f1w, _f1w))
 def _gamma_projection(prior_weight, prior_alpha, prior_beta, alpha, beta):
     """
     Given variational approximation to posterior: multiply by exact prior,
@@ -175,13 +191,14 @@ def _gamma_projection(prior_weight, prior_alpha, prior_beta, alpha, beta):
         # tlogt = np.sum(weight * E_tlogt)
         log_const[i] = norm
         alpha[i], beta[i] = approx.approximate_gamma_kl(t, logt)
+        # TODO: do something faster, like
         # beta[i] = 1 / (tlogt - t * logt)
         # alpha[i] = t * beta[i] - 1.0
 
     return log_const
 
 
-@numba.njit("Tuple((f8[:,:], f8[:,:]))(f8[:,:], f8[:,:], i4, f8, b1)")
+@numba.njit(_tuple((_f2w, _f2w, _f1w))(_f2r, _f2r, _i, _f, _b))
 def fit_gamma_mixture(mixture, observations, max_iterations, tolerance, verbose):
     """
     Run EM until relative tolerance or maximum number of iterations is
@@ -192,8 +209,13 @@ def fit_gamma_mixture(mixture, observations, max_iterations, tolerance, verbose)
     assert mixture.shape[1] == 3
     assert observations.shape[1] == 2
 
-    mix_weight, mix_alpha, mix_beta = mixture.T
-    alpha, beta = observations.T
+    # mix_weight, mix_alpha, mix_beta = np.hsplit(mixture, 3)
+    # alpha, beta = np.hsplit(observations, 2)
+    mix_weight = mixture[:, 0].copy()
+    mix_alpha = mixture[:, 1].copy()
+    mix_beta = mixture[:, 2].copy()
+    alpha = observations[:, 0].copy()
+    beta = observations[:, 1].copy()
 
     last = np.inf
     for itt in range(max_iterations):
@@ -210,8 +232,7 @@ def fit_gamma_mixture(mixture, observations, max_iterations, tolerance, verbose)
             break
 
     # conditional posteriors for each observation
-    # log_const = _gamma_projection(mix_weight, mix_alpha, mix_beta, alpha, beta)
-    _gamma_projection(mix_weight, mix_alpha, mix_beta, alpha, beta)
+    log_const = _gamma_projection(mix_weight, mix_alpha, mix_beta, alpha, beta)
 
     new_mixture = np.zeros(mixture.shape)
     new_observations = np.zeros(observations.shape)
@@ -221,7 +242,7 @@ def fit_gamma_mixture(mixture, observations, max_iterations, tolerance, verbose)
     new_mixture[:, 1] = mix_alpha
     new_mixture[:, 2] = mix_beta
 
-    return new_mixture, new_observations
+    return new_mixture, new_observations, log_const
 
 
 def initialize_mixture(parameters, num_components):
@@ -230,6 +251,8 @@ def initialize_mixture(parameters, num_components):
     "parameters" are in natural parameterization (not shape/rate)
     """
     global_prior = np.empty((num_components, 3))
+    if num_components == 0:
+        return global_prior
     num_nodes = parameters.shape[0]
     age_classes = np.tile(
         np.arange(num_components),