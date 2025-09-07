6bb0a4113af5197d8780eaadf24bb969c61ff751
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2025-09-07 23:52:58 +0100

    Move to pyproject

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    # The package name along with all the other metadata is specified in setup.cfg
-    # However, GitHub's dependency graph can't see the package unless we put this here.
-    name="tsdate",
-    packages=["tsdate"],
-)
